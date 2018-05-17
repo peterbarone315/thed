0329564552d41438c4d31d7e7b370040a5020f28
peterbarone315 <peter@cnycreativeconcepts.com>
2018-05-17 12:46:03 -0400

    added proxyURL testing simplecast feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -48,6 +50,7 @@ import { YtProvider } from '../providers/yt/yt';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
     
   ],
--- src/pages/ecradio/ecradio.ts
+++ src/pages/ecradio/ecradio.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -7,12 +9,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ecradio.html',
 })
 export class EcradioPage {
+  url: string
+  data: string
+  ecpodsUrl: string
+  ecpodCasts : any
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public http: HttpClient, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EcradioPage');
+    this.getEcradioPods();
   }
+
+  getEcradioPods(){
+    //https://rss.simplecast.com/podcasts/4228/rss
+    this.ecpodsUrl = 'https://rss.simplecast.com/podcasts/4228/rss';
+    return new Promise(resolve => {
+      this.http.get(this.ecpodsUrl).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+ }
 
 }
