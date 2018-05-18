0bdb8c3c5682501b95155b4da14e0da668ee7e97
peterbarone315 <peter@cnycreativeconcepts.com>
2018-05-18 13:46:07 -0400

    working XML Feed

--- src/pages/ecradio/ecradio.ts
+++ src/pages/ecradio/ecradio.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -15,6 +14,9 @@ export class EcradioPage {
   ecpodCasts : any
 
   constructor(public http: HttpClient, public navCtrl: NavController, public navParams: NavParams) {
+    this.ecpodsUrl = 'https://rss.simplecast.com/podcasts/4228/rss';
+    console.log('hello simple cast');
+
   }
 
   ionViewDidLoad() {
@@ -24,10 +26,12 @@ export class EcradioPage {
 
   getEcradioPods(){
     //https://rss.simplecast.com/podcasts/4228/rss
-    this.ecpodsUrl = 'https://rss.simplecast.com/podcasts/4228/rss';
     return new Promise(resolve => {
-      this.http.get(this.ecpodsUrl).subscribe(data => {
+      this.http.get(this.ecpodsUrl, {responseType:'text'}).subscribe(data => {
         resolve(data);
+        this.ecpodCasts=(data);
+        //JSON.stringify(data);
+        console.log(data);
       }, err => {
         console.log(err);
       });
