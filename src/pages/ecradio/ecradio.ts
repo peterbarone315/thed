d6e4e25176ba78ff71babc7533379144d9b46df4
peterbarone315 <peter@cnycreativeconcepts.com>
2018-05-29 10:25:57 -0400

    add youtube and podcast

--- src/pages/ecradio/ecradio.ts
+++ src/pages/ecradio/ecradio.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 
 @IonicPage()
 @Component({
@@ -12,13 +12,16 @@ import 'rxjs/add/operator/map';
 })
 export class EcradioPage {
    Ecpodcasts: any;
+   Ecvideos: any;
 
-  //https://api.simplecast.com/v1/podcasts/4228/episodes.json?api_key=sc_BLGQDzLUL53_jWHcWPVdsA'
+
   apiUrl = 'https://api.simplecast.com/v1/podcasts/4228/episodes.json?api_key=sc_BLGQDzLUL53_jWHcWPVdsA';
+  ytapiUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLueRqxTsQ74K4P3u-2O9434j9QiF3dKtT&key=AIzaSyCGiBjckOk0-vdJgOHrPpr75pOXgM_iJ4U';
+  
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) { 
     this.getECpods();
-    this.getECvideos();
+    this.getECvideolists();
   }
 
   getECpods(){
@@ -33,8 +36,16 @@ export class EcradioPage {
   });
 }
 
-  getECvideos(){
-
+  getECvideolists(){
+    return new Promise(resolve => {
+      this.httpClient.get(this.ytapiUrl).subscribe(ytdata => {
+        resolve(ytdata);
+        this.Ecvideos = ytdata;
+        console.log(ytdata);
+      }, err => {
+        console.log(err);
+      });
+    });
   }
  
 
